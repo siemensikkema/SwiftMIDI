a4ce8cdb6123c3d50a85943b7e7310d0d59dd4de
Siemen Sikkema <siemen@unclutter.nl>
2015-06-11 12:28:19 +0200

    Initial Commit

---
+++ SwiftMIDI/SwiftMIDI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMIDI.h
+//  SwiftMIDI
+//
+//  Created by Siemen Sikkema on 11/06/15.
+//  Copyright © 2015 Unclutter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMIDI.
+FOUNDATION_EXPORT double SwiftMIDIVersionNumber;
+
+//! Project version string for SwiftMIDI.
+FOUNDATION_EXPORT const unsigned char SwiftMIDIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMIDI/PublicHeader.h>
+
+
