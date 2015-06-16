11de3765832e57b1bd41c8d58b8809b782ff78dd
Siemen Sikkema <siemen@unclutter.nl>
2015-06-16 07:30:41 +0200

    Improve error handling
    - refactor status to SwiftMIDIError conversion to function
    - use defer for cleaner code
    - don't throw from init, use factory methods instead

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,117 +3,115 @@ import CoreMIDI
 
 XCPSetExecutionShouldContinueIndefinitely(true)
 
-protocol Initable {
-     init()
-}
+func evaluateStatus(closure: Void -> OSStatus) throws {
+    let status = closure()
 
-extension MIDIObjectRef: Initable {}
-
-func createMIDIObject<T: Initable>(createClosure: UnsafeMutablePointer<T> -> OSStatus) throws -> T {
-    var t = T()
-    let status = withUnsafeMutablePointer(&t) {
-        return createClosure($0)
-    }
-    if status == noErr {
-        return t
-    } else {
+    if status != noErr {
         throw SwiftMIDIError(status: status)
     }
+}
+
+func createMIDIObject<T>(createClosure: UnsafeMutablePointer<T> -> OSStatus) throws -> T {
+    let pointerToMIDIObject = UnsafeMutablePointer<T>.alloc(1)
+    defer { pointerToMIDIObject.destroy() }
+
+    try evaluateStatus { createClosure(pointerToMIDIObject) }
+
+    return pointerToMIDIObject.memory
 }
 
 func getMIDIObjectProperty<T>(createClosure: UnsafeMutablePointer<Unmanaged<T>?> -> OSStatus) throws -> T? {
     var t: Unmanaged<T>? = .None
-    let status = withUnsafeMutablePointer(&t) {
-        return createClosure($0)
+    defer { t?.release() }
+
+    try evaluateStatus {
+        withUnsafeMutablePointer(&t) {
+            return createClosure($0)
+        }
     }
-    if status == noErr {
-        return t?.takeUnretainedValue()
-    } else {
-        throw SwiftMIDIError(status: status)
-    }
+
+    return t?.takeUnretainedValue()
 }
 
 class Client {
-
-    let clientRef: MIDIObjectRef
+    let clientRef: MIDIClientRef
     var inputPorts: [InputPort] = []
 
-    init(clientName: String = "com.swiftmidi.client") throws {
-        do {
-            try clientRef = createMIDIObject {
-                return MIDIClientCreate(clientName, {
-                        let stateChange = MIDIStateChange(notificationPointer: $0.0)
-                        stateChange
-                    }, nil, $0)
-            }
-        } catch {
-            clientRef = 0
-            throw error
-        }
+    static func create(withName name: String = "com.swiftmidi.client") throws -> Client {
+        return try Client(clientRef: createMIDIObject {
+            return MIDIClientCreate(name, { (pointerToMIDINotification, _) in
+                MIDIStateChange(notificationPointer: pointerToMIDINotification)
+                }, nil, $0)
+        })
     }
 
-    var name: String? {
-        do {
-            let name = try getMIDIObjectProperty {
-                return MIDIObjectGetStringProperty(self.clientRef, kMIDIPropertyName, $0)
-            }
-            return name as String?
-        } catch {
-            return nil
+    init(clientRef: MIDIClientRef) {
+        self.clientRef = clientRef
+    }
+
+    func name() throws -> String? {
+        let name = try getMIDIObjectProperty {
+            return MIDIObjectGetStringProperty(self.clientRef, kMIDIPropertyName, $0)
         }
+        return name as String?
     }
 
     func addInputPort() throws {
-        if let name = name {
-            try inputPorts.append(InputPort(clientRef: clientRef, portName: "\(name).port\(inputPorts.count)"))
-        } else {
-            throw SwiftMIDIError.Unknown
-        }
+        try inputPorts.append(InputPort.create(withName: "\(name()).port\(inputPorts.count)", clientRef: clientRef))
     }
 }
 
-func packetsFromPacketListPointer(packetListPointer: UnsafePointer<MIDIPacketList>) -> [MIDIPacket] {
+func packetsFromPointerToPacketList(pointerToPacketList: UnsafePointer<MIDIPacketList>) -> [MIDIPacket] {
     var packets: [MIDIPacket] = []
+    let numPackets = pointerToPacketList.memory.numPackets // store before re-initing the packet list
+    let pointerToPacket = MIDIPacketListInit(UnsafeMutablePointer(pointerToPacketList))
 
-    let MIDIPacketPointer = MIDIPacketListInit(UnsafeMutablePointer(packetListPointer))
-
-    for _ in 0..<packetListPointer.memory.numPackets {
-        packets.append(MIDIPacketPointer.memory)
-        MIDIPacketNext(MIDIPacketPointer)
+    for _ in 0..<numPackets {
+        packets.append(pointerToPacket.memory)
+        MIDIPacketNext(pointerToPacket)
     }
+
+    pointerToPacket.destroy(Int(numPackets))
 
     return packets
 }
 
 class InputPort {
-
     let inputPortRef: MIDIPortRef
+    var sources: [MIDIEndpointRef] = []
 
-    init(clientRef: MIDIObjectRef, portName: String) throws {
-        do {
-            try inputPortRef = createMIDIObject {
-                return MIDIInputPortCreate(clientRef, portName, { (packetListPointer, _, _) in
-                    let packets = packetsFromPacketListPointer(packetListPointer)
+    static func create(withName name: String, clientRef: MIDIClientRef) throws -> InputPort {
+        return try InputPort(inputPortRef: createMIDIObject {
+            return MIDIInputPortCreate(clientRef, name, { (pointerToPacketList, _, _) in
 
-                    for packet in packets {
-                        packet.timeStamp
-                    }
-                    }, nil, $0)
-            }
-        } catch {
-            inputPortRef = 0
-            throw error
+                let packets = packetsFromPointerToPacketList(pointerToPacketList)
+
+                for packet in packets {
+                    packet.timeStamp
+                }
+                }, nil, $0)
+            })
+    }
+
+    init(inputPortRef: MIDIPortRef) {
+        self.inputPortRef = inputPortRef
+    }
+
+    func connectSource(source: MIDIEndpointRef) throws {
+        try evaluateStatus {
+            MIDIPortConnectSource(self.inputPortRef, source, nil)
         }
+
+        sources.append(source)
     }
 }
 
 do {
-    if MIDIGetNumberOfSources() > 0 {
-        let client = try Client()
-        try client.addInputPort()
+    let client = try Client.create()
+    try client.addInputPort()
 
-        let source = MIDIGetSource(0)
-        MIDIPortConnectSource(client.inputPorts.first!.inputPortRef, source, nil)
+    if let inputPort = client.inputPorts.first where MIDIGetNumberOfSources() > 0 {
+        try inputPort.connectSource(MIDIGetSource(0))
     }
 } catch {
     print(error)
